eb46e26287ba44009924ecff1363fef7e6c92b44
Shlok Kohli <shlokkohli11@gmail.com>
2024-12-07 19:54:06 +0530

    Get suggested users

--- frontend/src/components/common/RightPanel.jsx
+++ frontend/src/components/common/RightPanel.jsx
@@ -1,9 +1,25 @@
 import { Link } from "react-router-dom";
 import RightPanelSkeleton from "../skeletons/RightPanelSkeleton";
-import { USERS_FOR_RIGHT_PANEL } from "../../utils/db/dummy";
+import {useQuery, useMutation} from'@tanstack/react-query';
 
 const RightPanel = () => {
-	const isLoading = false;
+
+	const { data:suggestedUsers, isLoading } = useQuery({
+		queryKey: ['suggestedUsers'],
+		queryFn: async() => {
+			try {
+				const res = await fetch('/api/users/suggested')
+				const data = await res.json();
+
+				return data.data;
+			} catch (error) {
+				throw new Error(error);
+			}
+		}
+	})
+
+	console.log(suggestedUsers);
+	if(!suggestedUsers) return <div className="md:w-64 w-0"></div>
 
 	return (
 		<div className='hidden lg:block my-4 mx-2'>
@@ -20,10 +36,10 @@ const RightPanel = () => {
 						</>
 					)}
 					{!isLoading &&
-						USERS_FOR_RIGHT_PANEL?.map((user) => (
+						suggestedUsers?.map((user) => (
 							<Link
 								to={`/profile/${user.username}`}
-								className='flex items-center justify-between gap-4'
+								className='flex items-center justify-between gap-4 mt-4'
 								key={user._id}
 							>
 								<div className='flex gap-2 items-center'>
@@ -34,7 +50,7 @@ const RightPanel = () => {
 									</div>
 									<div className='flex flex-col'>
 										<span className='font-semibold tracking-tight truncate w-28'>
-											{user.fullName}
+											{user.fullname}
 										</span>
 										<span className='text-sm text-slate-500'>@{user.username}</span>
 									</div>
